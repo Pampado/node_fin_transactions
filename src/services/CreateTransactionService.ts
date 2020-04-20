f1717fe22323a43b265d74c8db98d91ba2a688dc
pampado <pampado@hotmail.com>
2020-04-20 04:31:44 -0400

    projeto finalizado

---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,40 @@
+import TransactionsRepository from '../repositories/TransactionsRepository'
+import Transaction from '../models/Transaction'
+
+interface Request {
+  title: string
+
+  value: number
+
+  type: 'income' | 'outcome'
+}
+
+class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository
+  }
+
+  public execute({ title, value, type }: Request): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Invalid transaction type.')
+    }
+
+    const { total } = this.transactionsRepository.getBalance()
+
+    if (type === 'outcome' && total < value) {
+      throw new Error('Not enought balance.')
+    }
+
+    const transction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    })
+
+    return transction
+  }
+}
+
+export default CreateTransactionService
